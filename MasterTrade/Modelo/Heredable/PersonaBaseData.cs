c752ba01f34b7462ab95b1a4521f1f371c4148a3
heyscar romero <heyscarromero@gmail.com>
2022-05-31 08:11:38 -0400

    modificacion modelo proveedores clientes add migrations  a gitignore

--- MasterTrade/Modelo/Heredable/PersonaBaseData.cs
+++ MasterTrade/Modelo/Heredable/PersonaBaseData.cs
@@ -21,17 +21,13 @@ namespace MasterTrade.Modelo.Heredable
         public string RazonSocial { get; set; }
         [MaxLength(200)]
         public string? Correo { get; set; }
-        [Column(Order = 4,TypeName = "Date")]
-        public DateTime FechaNacimento { get; set; }
         [MaxLength(20)]
         [Column(Order = 5)]
         public string? Telefono { get; set; }
         [MaxLength(250)]
         [Column(Order = 6)]
         public string? Direccion { get; set; }
-        [MaxLength(250)]
-        [Column(Order = 7)]
-        public string? Imagen { get; set; }
+
 
     }
 }
